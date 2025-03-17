5b2543c5358e1d531a8f712b1da51ff6302f3171
Onat Bulut <96723086+OnatBulut@users.noreply.github.com>
2025-03-17 21:34:01 +0300

    Delete rtl directory

--- rtl/program_counter_increment.sv
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module program_counter_increment(input  logic [31:0] pc_i,
-
-                                 output logic [31:0] pc_plus_4_o);
-
-    assign pc_plus_4_o = pc_i + 32'h4;
-
-endmodule
--- rtl/program_counter_target.sv
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module program_counter_target(input  logic [31:0] pc_i, imm_ext_i,
-
-                              output logic [31:0] pc_target_o);
-
-    assign pc_target_o = pc_i + imm_ext_i;
-
-endmodule
